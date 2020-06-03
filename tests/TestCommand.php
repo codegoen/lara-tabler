994412c9142d2d514417f2134c53cfd58aeec5f6
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-06-03 16:41:44 +0000

    Apply fixes from StyleCI

--- tests/TestCommand.php
+++ tests/TestCommand.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Support\Facades\Artisan;
-
 class TestCommand extends TestCase
 {
     /** @tests */
     public function testsMakeAuthCommand()
     {
         $this->artisan('tabler:make-auth');
-        
-        $this->assertTrue(is_file(__DIR__."/../src/Console/Commands/TablerAuthCommand.php"));
+
+        $this->assertTrue(is_file(__DIR__.'/../src/Console/Commands/TablerAuthCommand.php'));
     }
 }
